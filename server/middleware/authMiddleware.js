2a2ce5a638bba53b660de7756a30584c7bebc644
Suman-da97 <sumandass1811@gmail.com>
2026-06-05 09:24:08 +0530

    Fix token priority in authMiddleware to prevent cookie conflicts

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,10 +5,10 @@ class AuthMiddleware {
     static async protect(req, res, next) {
         try {
             let token;
-            if (req.cookies && req.cookies.token) {
-                token = req.cookies.token;
-            } else if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
                 token = req.headers.authorization.split(' ')[1];
+            } else if (req.cookies && req.cookies.token && req.cookies.token !== 'none') {
+                token = req.cookies.token;
             }
 
             if (!token) {
